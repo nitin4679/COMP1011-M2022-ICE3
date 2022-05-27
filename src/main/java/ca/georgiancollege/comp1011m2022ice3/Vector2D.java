e6d577e8ff115e94b837238f33cccd6986d423a2
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-05-27 18:25:42 -0400

    updated vector2D class with constructors that take id

--- src/main/java/ca/georgiancollege/comp1011m2022ice3/Vector2D.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice3/Vector2D.java
@@ -15,42 +15,63 @@ public class Vector2D {
 
     //Constructors
 
+    /**
+     * Default constructor
+     * - set x and y components to 0.0f and id to -1
+     */
     public Vector2D()
     {
-        m_x = 0.0f;
-        m_y = 0.0f;
+        m_id = -1;
+        setX(0.0f);
+        setY(0.0f);
     }
 
     /**
+     * This constructor takes x and y parameters
+     * - id is set to -1
      * @param x
      * @param y
      */
     public  Vector2D(float x, float y)
     {
-        m_x = x;
-        m_y = y;
+        m_id = -1;
+        setX(x);
+        setY(y);
+    }
+
+    public Vector2D(int id, float x, float y)
+    {
+        this(x, y);
+        setID(id);
     }
 
 
-    public float getM_x() {
-        // TODO implement here
-        return 0.0f;
-    }
-
-    /**
-     * @param value
-     */
-    public void setM_x(float value) {
-        // TODO implement here
-    }
+//    public float getM_x() {
+//        // TODO implement here
+//        return 0.0f;
+//    }
+//
+//
+//    public void setM_x(float value) {
+//        // TODO implement here
+//    }
 
     // public properties (Getters and setters)
 
-    public int getID() {
+    public int getID()
+    {
         return m_id;
     }
 
-    public void setID(int id) {
+    public void setID(int id)
+    {
+        if (id < 0)
+        {
+            throw new IllegalArgumentException("Vector ID must be greater than zero");
+        }else
+        {
+            m_id = id;
+        }
         m_id = id;
     }
 
