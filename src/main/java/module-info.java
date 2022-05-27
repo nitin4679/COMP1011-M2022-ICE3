12a25e7a534d556a9928cd055f596908decb7cf4
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-05-27 17:49:15 -0400

    Added support for jdbc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module ca.georgiancollege.comp1011m2022ice3 {
+module ca.georgiancollege.comp1011m2022ice3
+{
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
-
-    opens ca.georgiancollege.comp1011m2022ice3 to javafx.fxml;
+    opens  ca.georgiancollege.comp1011m2022ice3 to javafx.fxml;
     exports ca.georgiancollege.comp1011m2022ice3;
 }
